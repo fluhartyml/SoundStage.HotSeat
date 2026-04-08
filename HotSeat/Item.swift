2ef75220eba96fc7c3c1295f1fe2bed25d542ad6
Michael Fluharty <michael@fluharty.com>
2026-04-08 12:10:59 -0500

    Initial Commit

---
+++ HotSeat/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HotSeat
+//
+//  Created by Michael Fluharty on 4/8/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
